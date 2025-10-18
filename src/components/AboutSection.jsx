f62572d643fe148af99b3e80f6021978f65999d1
Banti-Kushwaha <151020944+Banti-Kushwaha@users.noreply.github.com>
2025-10-18 20:14:34 +0530

    second commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -43,7 +43,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1jWlP8xjRbbfFtPFQjxLqWwrpOuWR96qE/view?usp=drivesdk"
+                href="https://drive.google.com/file/d/10GECYPAVlFqoV7JJDZJJtw1Bl_oTRstD/view?usp=drivesdk"
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
