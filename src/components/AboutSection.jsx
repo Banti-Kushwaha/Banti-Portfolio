de6ec47e4d915fe3d6dc7d2c85cb3099aae37c17
Banti-Kushwaha <151020944+Banti-Kushwaha@users.noreply.github.com>
2026-01-14 23:44:57 +0530

    Add shadow and scale effects to buttons

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,7 +37,7 @@ export const AboutSection = () => {
               <a
                 href="https://www.linkedin.com/in/banti-kumar-534997265/"
                 target="_blank"
-                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 shadow-lg hover:scale-105 transition-colors duration-300"
               >
                 LinkedIn Profile
               </a>
@@ -45,7 +45,7 @@ export const AboutSection = () => {
               <a
                 href="https://drive.google.com/file/d/10GECYPAVlFqoV7JJDZJJtw1Bl_oTRstD/view?usp=drivesdk"
                 target="_blank"
-                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 shadow-lg hover:scale-105 transition-colors duration-300"
               >
                 Download CV
               </a>
